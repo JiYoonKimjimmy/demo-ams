b57b609e7dc39d275f3449f9e11f940c982e4085
jimmyberg <jimmyberg.kim@gmail.com>
2023-09-16 18:00:44 +0900

    fix: HelloController.kt 수정

--- src/main/kotlin/me/jimmyberg/ams/api/controller/HelloController.kt
+++ src/main/kotlin/me/jimmyberg/ams/api/controller/HelloController.kt
@@ -1,4 +1,14 @@
 package me.jimmyberg.ams.api.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello World"
+    }
+
 }
