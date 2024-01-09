5c529bbb8c9e45084f146074dd76a20e85940edb
jimmyberg <jimmyberg.kim@gmail.com>
2024-01-09 15:03:37 +0900

    fix: 학생 정보 domain 클래스 수정

--- src/main/kotlin/me/jimmyberg/ams/student/domain/Student.kt
+++ src/main/kotlin/me/jimmyberg/ams/student/domain/Student.kt
@@ -1,7 +1,33 @@
 package me.jimmyberg.ams.student.domain
 
 data class Student(
-    private val name: String
-) {
-    fun getName() = this.name
+    val id: String,
+    val name: String,
+    val phone: String,
+    val birthday: String,
+    val gender: Gender,
+    val address: String?,
+    val schoolName: String,
+    val schoolType: SchoolType,
+    val grade: Int,
+    val status: StudentStatus
+)
+
+enum class SchoolType {
+    PRIMARY,
+    MIDDLE,
+    HIGH,
+    UNIVERSITY
+}
+
+enum class StudentStatus {
+    REGISTER_WAITING,
+    ACTIVATED,
+    INACTIVATED
+}
+
+enum class Gender {
+    MALE,
+    FEMALE,
+    NON_BINARY
 }
