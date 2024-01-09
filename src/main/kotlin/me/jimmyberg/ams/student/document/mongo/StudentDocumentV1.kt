9d4b49a889fcc304f01c0844cf9bae54a6daf28f
jimmyberg <jimmyberg.kim@gmail.com>
2024-01-09 15:03:57 +0900

    fix: 학생 정보 mongo document 클래스 수정

--- src/main/kotlin/me/jimmyberg/ams/student/document/mongo/StudentDocumentV1.kt
+++ src/main/kotlin/me/jimmyberg/ams/student/document/mongo/StudentDocumentV1.kt
@@ -1,5 +1,10 @@
 package me.jimmyberg.ams.student.document.mongo
 
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import me.jimmyberg.ams.student.domain.Gender
+import me.jimmyberg.ams.student.domain.SchoolType
+import me.jimmyberg.ams.student.domain.StudentStatus
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
@@ -7,5 +12,16 @@ import org.springframework.data.mongodb.core.mapping.Document
 data class StudentDocumentV1(
     @Id
     val id: String? = null,
-    val name: String
+    val name: String,
+    val phone: String,
+    val birthday: String,
+    @Enumerated(EnumType.STRING)
+    val gender: Gender,
+    val address: String?,
+    val schoolName: String,
+    @Enumerated(EnumType.STRING)
+    val schoolType: SchoolType,
+    val grade: Int,
+    @Enumerated(EnumType.STRING)
+    val status: StudentStatus
 )
