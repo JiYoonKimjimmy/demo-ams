fef2661b1e33a95f87f43222580abd332b692feb
jimmyberg <jimmyberg.kim@gmail.com>
2024-01-14 17:55:34 +0900

    chore: StudentRepository unused import 삭제

--- src/main/kotlin/me/jimmyberg/ams/student/repository/StudentRepository.kt
+++ src/main/kotlin/me/jimmyberg/ams/student/repository/StudentRepository.kt
@@ -1,6 +1,5 @@
 package me.jimmyberg.ams.student.repository
 
-import me.jimmyberg.ams.student.document.mongo.StudentDocumentV1
 import me.jimmyberg.ams.student.domain.Student
 import me.jimmyberg.ams.student.domain.StudentMapper
 import me.jimmyberg.ams.student.repository.mongo.StudentMongoRepository
