d3e05e333b32ebd4be779b072630803ce067b0ed
jimmyberg <jimmyberg.kim@gmail.com>
2024-03-09 18:09:52 +0900

    refactor: UpdateStudentServiceBootTest 클래스 수정
    - 학생 정보 업데이트 테스트 코드 수정

--- src/test/kotlin/me/jimmyberg/ams/student/service/UpdateStudentServiceBootTest.kt
+++ src/test/kotlin/me/jimmyberg/ams/student/service/UpdateStudentServiceBootTest.kt
@@ -1,26 +1,52 @@
 package me.jimmyberg.ams.student.service
 
 import me.jimmyberg.ams.common.domain.Address
+import me.jimmyberg.ams.common.enumerate.Gender
+import me.jimmyberg.ams.common.enumerate.SchoolType
+import me.jimmyberg.ams.common.enumerate.StudentStatus
 import me.jimmyberg.ams.student.controller.model.StudentModelMapper
+import me.jimmyberg.ams.student.domain.School
+import me.jimmyberg.ams.student.domain.Student
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class UpdateStudentServiceBootTest(
-    @Autowired private val findStudentService: FindStudentServiceV1,
+    @Autowired private val saveStudentService: SaveStudentServiceV1,
     @Autowired private val updateStudentService: UpdateStudentServiceV1,
     @Autowired private val mapper: StudentModelMapper
 ) {
 
-    @Test
-    fun `학생 정보 조회 후 주소 변경하여 업데이트 저장한다`() {
+    companion object {
+        @JvmStatic
+        fun student(): Set<Student> {
+            val student = Student(
+                name = "김모아",
+                phone = "01012341234",
+                birthday = "19900202",
+                gender = Gender.FEMALE,
+                school = School(
+                    schoolName = "여의도중학교",
+                    schoolType = SchoolType.MIDDLE,
+                    grade = 1
+                ),
+                status = StudentStatus.REGISTER_WAITING
+            )
+            return setOf(student)
+        }
+    }
+
+    @MethodSource("student")
+    @ParameterizedTest
+    fun `학생 정보 조회 후 주소 변경하여 업데이트 저장한다`(domain: Student) {
         // given
-        val studentId = "65abefa1cf7fcb5a102ccdea"
         val address = Address("12345", "서울시 영등포구 은행로 25", "Hello World")
-        val student = findStudentService
-            .findOne(studentId)
+        val saved = saveStudentService.save(domain)
+        val student = saved
             .apply {
                 this.zipCode = address.zipCode
                 this.baseAddress = address.baseAddress
@@ -28,10 +54,12 @@ class UpdateStudentServiceBootTest(
             }
 
         // when
-        val result = updateStudentService.update(mapper.modelToDomain(student))
+        val updated = updateStudentService.update(mapper.modelToDomain(student))
 
         // then
-        assertNotNull(result)
+        assertNotNull(updated)
+        assertEquals(updated.id, saved.id)
+        assertNotNull(updated.address)
     }
 
 }
