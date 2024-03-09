4331d9bcfe845b540b768747df9371d9d0323d3e
jimmyberg <jimmyberg.kim@gmail.com>
2024-03-09 18:21:32 +0900

    refactor: SaveStudentServiceBootTest 클래스 수정
    - studentRepository 활용한 테스트 코드 작성으로 변경

--- src/test/kotlin/me/jimmyberg/ams/student/service/SaveStudentServiceBootTest.kt
+++ src/test/kotlin/me/jimmyberg/ams/student/service/SaveStudentServiceBootTest.kt
@@ -2,9 +2,11 @@ package me.jimmyberg.ams.student.service
 
 import me.jimmyberg.ams.common.enumerate.Gender
 import me.jimmyberg.ams.common.enumerate.SchoolType
-import me.jimmyberg.ams.student.controller.model.SaveStudentRequest
-import me.jimmyberg.ams.student.controller.model.StudentModel
+import me.jimmyberg.ams.common.enumerate.StudentStatus
+import me.jimmyberg.ams.student.controller.model.StudentModelMapper
+import me.jimmyberg.ams.student.domain.School
 import me.jimmyberg.ams.student.domain.Student
+import me.jimmyberg.ams.student.repository.StudentRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.params.ParameterizedTest
@@ -14,45 +16,40 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class SaveStudentServiceBootTest(
-    @Autowired private val saveStudentService: SaveStudentServiceV1
+//    @Autowired private val saveStudentService: SaveStudentServiceV1
+    @Autowired private val studentRepository: StudentRepository,
+    @Autowired private val mapper: StudentModelMapper
 ) {
 
     companion object {
         @JvmStatic
-        fun saveStudentRequest(): Set<SaveStudentRequest> {
-            val student = StudentModel(
+        fun student(): Set<Student> {
+            val student = Student(
                 name = "김모아",
                 phone = "01012341234",
                 birthday = "19900202",
                 gender = Gender.FEMALE,
-                schoolName = "여의도중학교",
-                schoolType = SchoolType.MIDDLE,
-                grade = 1
+                school = School(
+                    schoolName = "여의도중학교",
+                    schoolType = SchoolType.MIDDLE,
+                    grade = 1
+                ),
+                status = StudentStatus.REGISTER_WAITING
             )
-            return setOf(SaveStudentRequest(student))
+            return setOf(student)
         }
     }
 
-    @MethodSource("saveStudentRequest")
+    @MethodSource("student")
     @ParameterizedTest
-    fun `신규 학생 정보 입력받아 저장한다`(request: SaveStudentRequest) {
-        // given
-        val domain = Student(
-            name = request.student.name,
-            phone = request.student.phone,
-            birthday = request.student.birthday,
-            gender = request.student.gender,
-            address = request.student.address,
-            school = request.student.school,
-            status = request.student.status
-        )
-
+    fun `신규 학생 정보 입력받아 저장한다`(student: Student) {
         // when
-        val saved = saveStudentService.save(domain)
+        val saved = studentRepository.save(student).let(mapper::domainToModel)
 
         // then
         assertNotNull(saved)
-        assertEquals(saved.name, request.student.name)
+        assertNotNull(saved.id)
+        assertEquals(saved.name, student.name)
     }
 
 }
