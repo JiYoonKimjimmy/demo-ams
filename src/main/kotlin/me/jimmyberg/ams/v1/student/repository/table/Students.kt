3037c4768a405563c9fa1cf154ec445fba3fff0e
jimmyberg <jimmyberg.kim@gmail.com>
2025-03-15 09:54:12 +0900

    fix: `Students` table > LongIdTable 변경

--- src/main/kotlin/me/jimmyberg/ams/v1/student/repository/table/Students.kt
+++ src/main/kotlin/me/jimmyberg/ams/v1/student/repository/table/Students.kt
@@ -1,19 +1,18 @@
 package me.jimmyberg.ams.v1.student.repository.table
 
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.dao.id.LongIdTable
 
-object Students : Table("students") {
-    val id = integer("id").autoIncrement()
-    val name = varchar("name", 128)
-    val indexOfName = integer("indexOfName")
-    val phone = varchar("phone", 32)
-    val birth = varchar("birth", 8)
-    val gender = varchar("gender", 5)
-    val zipCode = varchar("zipCode", 6)
-    val baseAddress = varchar("baseAddress", 128)
+object Students : LongIdTable("students") {
+    val name          = varchar("name", 128)
+    val indexOfName   = integer("indexOfName")
+    val phone         = varchar("phone", 32)
+    val birth         = varchar("birth", 8)
+    val gender        = varchar("gender", 5)
+    val zipCode       = varchar("zipCode", 6)
+    val baseAddress   = varchar("baseAddress", 128)
     val detailAddress = varchar("detailAddress", 128)
-    val schoolName = varchar("schoolName", 128)
-    val schoolType = varchar("schoolType", 20)
-    val grade = integer("grade")
-    val status = varchar("status", 20)
+    val schoolName    = varchar("schoolName", 128)
+    val schoolType    = varchar("schoolType", 20)
+    val grade         = integer("grade")
+    val status        = varchar("status", 20)
 }
