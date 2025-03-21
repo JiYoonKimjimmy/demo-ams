8ae4cfdca75fddbea03b244faba9aa163513fc4f
jimmyberg <jimmyberg.kim@gmail.com>
2025-03-21 20:54:12 +0900

    refactor: `student_parents` 관련 exposed table 클래스명 변경

--- src/main/kotlin/me/jimmyberg/ams/v1/relation/repository/entity/StudentParentTable.kt
+++ src/main/kotlin/me/jimmyberg/ams/v1/relation/repository/entity/StudentParentTable.kt
@@ -1,13 +1,13 @@
 package me.jimmyberg.ams.v1.relation.repository.entity
 
-import me.jimmyberg.ams.v1.parent.repository.entity.Parents
+import me.jimmyberg.ams.v1.parent.repository.entity.ParentTable
 import me.jimmyberg.ams.v1.student.repository.entity.StudentTable
 import org.jetbrains.exposed.sql.Table
 
-object StudentParents : Table("student_parents") {
+object StudentParentTable : Table("student_parents") {
     
     val studentId = long("student_id").references(StudentTable.id)
-    val parentId = long("parent_id").references(Parents.id)
+    val parentId = long("parent_id").references(ParentTable.id)
 
     override val primaryKey = PrimaryKey(studentId, parentId, name = "pk_student_parent")
 
