916ba5fe1ccd4df99ab6d05858f29faed9ce7b87
jimmyberg <jimmyberg.kim@gmail.com>
2025-09-14 13:52:09 +0900

    refactor: EntityNotFoundException > ResourceNotFoundException 예외 클래스 활용하도록 개선

--- src/main/kotlin/me/jimmyberg/ams/domain/service/StudentFindServiceImpl.kt
+++ src/main/kotlin/me/jimmyberg/ams/domain/service/StudentFindServiceImpl.kt
@@ -1,12 +1,13 @@
 package me.jimmyberg.ams.domain.service
 
-import jakarta.persistence.EntityNotFoundException
+import me.jimmyberg.ams.common.error.ErrorCode
+import me.jimmyberg.ams.common.error.exception.ResourceNotFoundException
+import org.springframework.stereotype.Service
 import me.jimmyberg.ams.common.model.ScrollResult
 import me.jimmyberg.ams.domain.model.Student
 import me.jimmyberg.ams.domain.model.predicate.StudentPredicate
 import me.jimmyberg.ams.domain.port.inbound.StudentFindService
 import me.jimmyberg.ams.domain.port.outbound.StudentRepository
-import org.springframework.stereotype.Service
 
 @Service
 class StudentFindServiceImpl(
@@ -15,7 +16,7 @@ class StudentFindServiceImpl(
 
     override fun findOne(predicate: StudentPredicate): Student {
         return studentRepository.findByPredicate(predicate)
-            ?: throw EntityNotFoundException("Student not found")
+            ?: throw ResourceNotFoundException(ErrorCode.STUDENT_NOT_FOUND)
     }
 
     override fun scroll(predicate: StudentPredicate): ScrollResult<Student> {
